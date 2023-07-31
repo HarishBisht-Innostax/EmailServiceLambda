aca1ff9d50541e5d3ec14a4d23d5c97aa8b6a720
Harish-Bisht <harish.bisht@innostax.com>
2023-07-31 17:15:38 +0530

    Refactored code to work for email only.

--- index.js
+++ index.js
@@ -3,78 +3,57 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const EMAIL_QUEUE_KEY = "demo-queue::/";
+const BASE_64 = "base64";
 
 const sendEmail = async (messageData) => {
   const { from, to, subject, html } = messageData;
   const emailMessage = { from, to, subject, html };
 
+  const { EMAIL_HOST, EMAIL_PORT, EMAIL_USERNAME, EMAIL_PASSWORD } =
+    process.env;
+
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
+      host: EMAIL_HOST,
+      port: EMAIL_PORT,
+      auth: { user: EMAIL_USERNAME, pass: EMAIL_PASSWORD },
     });
 
     const success = await transporter.sendMail(emailMessage);
-    return success?.response
-      ? "Successfully sent email: Response: " + success?.response
-      : "Unable to sent email";
+
+    if (success?.response) return "Successfully sent email.";
+    else throw new Error("Unable to send email.");
   } catch (err) {
-    console.log("Unable to sent email", err);
-    return `Unable to sent email ${err}`;
+    console.error("Error sending email:", err);
+    throw new Error("Unable to send email.");
   }
 };
 
 exports.handler = async (event) => {
   try {
-    const bufferObj = Buffer.from(
-      event.rmqMessagesByQueue[EMAIL_QUEUE_KEY][0].data,
-      "base64"
-    );
+    const queueData = event.rmqMessagesByQueue[EMAIL_QUEUE_KEY][0];
+    const bufferObj = Buffer.from(queueData.data, BASE_64);
 
     const messageData = JSON.parse(bufferObj.toString("utf8"));
 
-    let result = {};
-
-    const { num1, num2, operation } = messageData;
-
-    switch (operation) {
-      case "multiply":
-        result = num1 * num2;
-        break;
-
-      case "sum":
-        result = num1 + num2;
-        break;
-
-      default:
-        result = 0;
-        break;
-    }
-
     const emailResponse = await sendEmail(messageData);
 
-    const replyBackTo =
-      event.rmqMessagesByQueue[EMAIL_QUEUE_KEY][0].basicProperties.replyTo;
-    const correlationId =
-      event.rmqMessagesByQueue[EMAIL_QUEUE_KEY][0].basicProperties
-        .correlationId;
+    const basicProperties = queueData.basicProperties;
+
+    const { correlationId, replyTo: replyBackTo } = basicProperties;
 
     const connection = await amqp.connect(process.env.RABBIT_URL);
     const channel = await connection.createChannel();
 
     channel.sendToQueue(
       replyBackTo,
-      Buffer.from(JSON.stringify({ result, emailResponse })),
+      Buffer.from(JSON.stringify({ emailResponse })),
       { correlationId }
     );
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify({ result, emailResponse }),
+      body: JSON.stringify({ emailResponse }),
     };
     return response;
   } catch (err) {
